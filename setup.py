b073bbbd94d83b96d62d2ad6e4a84aeab371a6cc
Maghish <mail4mithran@gmail.com>
2024-07-27 19:52:21 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup, find_packages
 
+
+VERSION = '0.0.1'
+DESCRIPTION = 'RAGE is now officially on supported on Python.'
+LONG_DESCRIPTION = 'A package to manage your cloud database operations through local storage approach.'
+
+# Setting up
 setup(
-  name='rage_py',
-  version='0.1',
-  packages=find_packages(),
-  install_requires=[],
+    name="rage_py",
+    version=VERSION,
+    author="Maghish",
+    author_email="<mail4mithran@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'rage', 'rage-py', 'rage_py', 'ragepy', 'database'],
 )
