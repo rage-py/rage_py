4bccc2aae6e24a6fb61460098d6b9acf98da4937
Maghish <mail4mithran@gmail.com>
2024-07-28 11:21:36 +0530

    Changed tab size from 2 to 4

--- rage_py/rage.py
+++ rage_py/rage.py
@@ -4,37 +4,37 @@ import threading
 import sys
 
 def greet():
-  # greetMsg = returnHello()
-  time.sleep(5)
-  print("Hello world by Python 🐍")
+    # greetMsg = returnHello()
+    time.sleep(5)
+    print("Hello world by Python 🐍")
 
 loopActive = False
 
 def runLoop():
-  global loopActive
-  loopActive = True
-  index = 0
-  while loopActive:
-    time.sleep(3)
-    print(f"[{index}]")
-    index += 1
+    global loopActive
+    loopActive = True
+    index = 0
+    while loopActive:
+        time.sleep(3)
+        print(f"[{index}]")
+        index += 1
 
 loopThread = threading.Thread(target=runLoop, args=())
 
 def main():
-  global loopActive 
-  loopThread.start()
-  greet()
+    global loopActive 
+    loopThread.start()
+    greet()
 
-  try:
-    while True:
-      pass
-  except KeyboardInterrupt:
-    loopActive = False
-    loopThread.join()
-    sys.exit(0)
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        loopActive = False
+        loopThread.join()
+        sys.exit(0)
     
   
 
 if __name__ == '__main__':
-  main()
+    main()
