43ee397c2653999b017d8e0ed63724f18e13ba0d
Maghish <mail4mithran@gmail.com>
2024-07-28 11:04:33 +0530

    Experimenting multi-threads

--- rage_py/rage.py
+++ rage_py/rage.py
@@ -1,5 +1,40 @@
-from .util.hello import returnHello
+# from .util.hello import returnHello
+import time
+import threading
+import sys
 
 def greet():
-  greetMsg = returnHello()
-  print(greetMsg)
+  # greetMsg = returnHello()
+  time.sleep(5)
+  print("Hello world by Python 🐍")
+
+loopActive = False
+
+def runLoop():
+  global loopActive
+  loopActive = True
+  index = 0
+  while loopActive:
+    time.sleep(3)
+    print(f"[{index}]")
+    index += 1
+
+loopThread = threading.Thread(target=runLoop, args=())
+
+def main():
+  global loopActive 
+  loopThread.start()
+  greet()
+
+  try:
+    while True:
+      pass
+  except KeyboardInterrupt:
+    loopActive = False
+    loopThread.join()
+    sys.exit(0)
+    
+  
+
+if __name__ == '__main__':
+  main()
