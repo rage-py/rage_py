14d47a7ff5a7031a3acedd37697750eb6b004b9d
Maghish <mail4mithran@gmail.com>
2024-07-30 10:36:55 +0530

    Doing something

--- rage_py/methods/PushAfterInterval.py
+++ rage_py/methods/PushAfterInterval.py
@@ -1,6 +1,18 @@
+import time
+
 class PushAfterInterval():
     def __init__(self):
-        ...
+        self.loopActive = False
 
-    def sayHello(self):
-        print("Hello!")
+    def start(self):
+        self.loopActive = True
+    
+        index = 0
+        while self.loopActive:
+            time.sleep(3)
+            print(f"[{index}]")
+            index += 1
+
+    def stop(self):
+        self.loopActive = False
+        return
--- rage_py/rage.py
+++ rage_py/rage.py
@@ -1,34 +1,23 @@
 from .methods.PushAfterInterval import PushAfterInterval
-import time
 import threading
 
 class App():
     def __init__(self):
-        self.loopActive = False
-        # Private variable
-        self.__loopThread = threading.Thread(target=self.runLoop, args=())
-
-    def runLoop(self):
-        self.loopActive = True
-    
-        index = 0
-        while self.loopActive:
-            time.sleep(3)
-            print(f"[{index}]")
-            index += 1
-
+        # Private variables
+        self.__newMethodInstance = PushAfterInterval() 
+        self.__loopThread = threading.Thread(target=self.__newMethodInstance.start, args=())
 
     def main(self):     
         self.__loopThread.start()
-        newInstance = PushAfterInterval()
-        newInstance.sayHello()
 
     def returnLoopactive(self):
         return self.loopActive
     
     def stop(self):
-        self.loopActive = False
+        print("Application will terminate in few seconds")
+        self.__newMethodInstance.stop()
         self.__loopThread.join()
+        print("Application terminated successfully")
         return
 
 if __name__ == '__main__':
