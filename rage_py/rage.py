365744189b1b1517440c9404935380622ed23128
Maghish <mail4mithran@gmail.com>
2024-07-31 11:39:10 +0530

    Modified rage.py

--- rage_py/rage.py
+++ rage_py/rage.py
@@ -1,23 +1,33 @@
 from .methods.PushAfterInterval import PushAfterInterval
+from .util.formatLog import formatLog
 import threading
 
 class App():
-    def __init__(self):
+    def __init__(self, configFilePath: str, logger: bool = False):
+        # Public variables 
+        self.logger = logger
+        self.configFilePath = configFilePath
         # Private variables
         self.__newMethodInstance = PushAfterInterval() 
         self.__loopThread = threading.Thread(target=self.__newMethodInstance.start, args=())
+        self.__setup = False
 
-    def main(self):     
-        self.__loopThread.start()
+    def start(self):     
+        try:
+            if self.__setup is False:
+                ...
+            
+            self.__loopThread.start()
+        except:
+            formatLog("Unexpected error occurred!", "error", self.logger)
 
     def returnLoopactive(self):
         return self.loopActive
     
     def stop(self):
-        print("Application will terminate in few seconds")
         self.__newMethodInstance.stop()
         self.__loopThread.join()
-        print("Application terminated successfully")
+        formatLog("Application terminated successfully", "final", self.logger)
         return
 
 if __name__ == '__main__':
